853af4e18143c06672819ba65980fb6e4a9ee757
Javier Ojeda <ojeda.javier94@gmail.com>
2017-01-11 14:05:33 -0600

    create separated file to production on port 80

---
+++ server/production.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let express = require('express');
+let app = express();
+let mongoose = require('mongoose');
+// models
+let authors = require('./routes/authors');
+let posts = require('./routes/posts');
+// use a loger for requests
+let logger = require('./logger');
+app.use(logger);
+
+// connect to database
+mongoose.connect('mongodb://localhost/boilerblog');
+
+// serve static files from client/dist folder
+app.use(express.static(__dirname + '/../client/dist'));
+
+// models routes
+app.use('/authors', authors);
+app.use('/posts', posts);
+
+// start server
+app.listen(80, () => {
+  console.log(Date());
+  console.log('Amazing app is on port 80');
+});
